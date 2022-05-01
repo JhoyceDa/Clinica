b34d3930ca1c7b0765643b5fd69b65183d2ffe4f
unknown <miguelguevaraalejandro@gmail.com>
2022-05-01 13:58:35 -0500

    reto 5 javascript
    correccion url inicio

---
+++ utils/doctores.js
@@ -0,0 +1,171 @@
+export const doctores = [
+    {
+      "nombre": "Jacqueline Abad Nuñez",
+      "cmp": "CMP: 71927",
+      "sede":{
+          "id":1,
+          "nombre":"Lima"
+        },
+      "especialidad":{
+        "id":2,
+        "nombre": "Alergología",
+    },
+      "enlace":
+        "https://sabrmeamundociprod.blob.core.windows.net/bscbrmeamundociprod/Foto/1005569.jpg",
+    },
+    {
+        "nombre": "Segundo Carlos Abanto Argomedo",
+        "cmp": "CMP: 031326",
+        "sede":{
+            "id":1,
+            "nombre":"Lima"
+          },
+        "especialidad":{
+            "id":4,
+            "nombre": "Cardiología Pediatrica",
+        },
+        "enlace":
+          "https://sabrmeamundociprod.blob.core.windows.net/bscbrmeamundociprod/Foto/8712.jpg",
+    },
+    {
+        "nombre": "Marcela Abarca Benavente",
+        "cmp": "CMP: 036208",
+        "sede":{
+            "id":4,
+            "nombre":"Surco"
+          },
+        "especialidad":{
+        "id":4,
+        "nombre": "Surco",
+    },
+        "enlace":
+          "https://sabrmeamundociprod.blob.core.windows.net/bscbrmeamundociprod/Foto/5001.jpg",
+    },
+    {
+        "nombre": "Arturo Aguilar Maldonado",
+        "cmp": "CMP: 53507",
+        "sede":{
+            "id":4,
+            "nombre":"Surco"
+          },
+        "especialidad":{
+            "id":2,
+            "nombre": "Alergología",
+        },
+        "enlace":
+          "https://sabrmeamundociprod.blob.core.windows.net/bscbrmeamundociprod/Foto/1005108.jpg",
+    },
+    {
+        "nombre": "Sergio Ricardo Aguilar Romero",
+        "cmp": "CMP: 030230",
+        "sede":{
+            "id":1,
+            "nombre":"Lima"
+          },
+        "especialidad":{
+        "id":6,
+        "nombre": "Cirugía Bariátrica",
+    },
+        "enlace":
+          "https://sabrmeamundociprod.blob.core.windows.net/bscbrmeamundociprod/Foto/8083.jpg",
+    },
+    {
+        "nombre": "Carlos Roberto Aguirre Masson",
+        "cmp": "CMP: 49392",
+        "sede":{
+        "id":3,
+        "nombre": "San borja",
+    },
+        "especialidad":{
+        "id":2,
+        "nombre": "Alergología",
+    },
+        "enlace":
+          "https://sabrmeamundociprod.blob.core.windows.net/bscbrmeamundociprod/Foto/1005101.jpg",
+    },
+    {
+        "nombre": "Cesar Anibal Alarcon Olivera",
+        "cmp": "CMP: 20029",
+        "sede":{
+            "id":1,
+            "nombre":"Lima"
+          },
+        "especialidad":{
+        "id":6,
+        "nombre": "Cirugía Bariátrica",
+    },
+        "enlace":
+          "https://sabrmeamundociprod.blob.core.windows.net/bscbrmeamundociprod/Foto/5028.jpg",
+    },
+    {
+        "nombre": "Jose Samuel Alarcon Vera",
+        "cmp": "CMP: 023670",
+        "sede":{
+        "id":3,
+        "nombre": "San borja",
+    },
+        "especialidad":{
+            "id":8,
+            "nombre": "Cirugía De Cabeza y Cuello",
+        },
+        "enlace":
+          "https://www.clinicainternacional.com.pe/staff-medico-img/Foto/8387.jpg",
+    },
+    {
+        "nombre": "Ricardo Aliendres Galindo",
+        "cmp": "CMP: 082851",
+        "sede":{
+            "id":4,
+            "nombre": "Surco",
+        },
+        "especialidad":{
+        "id":4,
+        "nombre": "Surco",
+    },
+        "enlace":
+          "https://sabrmeamundociprod.blob.core.windows.net/bscbrmeamundociprod/Foto/1004966.jpg",
+    },
+    {
+        "nombre": "Olenka Alcas Arce",
+        "cmp": "CMP: 62023",
+        "sede":{
+            "id":1,
+            "nombre":"Lima"
+          },
+        "especialidad":{
+        "id":2,
+        "nombre": "Alergología",
+    },
+        "enlace":
+          "https://sabrmeamundociprod.blob.core.windows.net/bscbrmeamundociprod/Foto/1005459.jpg",
+    },
+    {
+        "nombre": "Patricia Aldana Torres",
+        "cmp": "CMP: 64243",
+        "sede":{
+        "id":3,
+        "nombre": "San borja",
+    },
+        "especialidad":{
+            "id":11,
+            "nombre": "Cirugía de Mano",
+        },
+        "enlace":
+          "https://sabrmeamundociprod.blob.core.windows.net/bscbrmeamundociprod/Foto/1005457.jpg",
+    },
+    {
+        "nombre": "Franklin Aldecoa Bedoya",
+        "cmp": "CMP: 20944",
+        "sede":{
+            "id":2,
+            "nombre": "Mediocentro San Isidro",
+        },
+        "especialidad":{
+            "id":10,
+            "nombre": "Cirugía de Mamas y Tumores",
+        },
+        "enlace":
+          "https://sabrmeamundociprod.blob.core.windows.net/bscbrmeamundociprod/Foto/1004452.jpg",
+    }
+
+];
