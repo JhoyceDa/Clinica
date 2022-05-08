dc4afacd825b74c89589e87fdc803b6fe1a8be5b
unknown <miguelguevaraalejandro@gmail.com>
2022-05-07 21:15:22 -0500

    funcionalidad registro usuario

---
+++ utils/especialidades.js
@@ -0,0 +1,47 @@
+export const especialidades = [
+    {
+        "id":1,
+        "nombre": "Administración",
+    },
+    {
+        "id":2,
+        "nombre": "Alergología",
+    },
+    {
+        "id":3,
+        "nombre": "Cardiología",
+    },
+    {
+        "id":4,
+        "nombre": "Cardiología Pediatrica",
+    },
+    {
+        "id":5,
+        "nombre": "Centro De endoscopia",
+    },
+    {
+        "id":6,
+        "nombre": "Cirugía Bariátrica",
+    },
+    {
+        "id":7,
+        "nombre": "Cirugía Cardiovascular-Tórax",
+    },
+    {
+        "id":8,
+        "nombre": "Cirugía De Cabeza y Cuello",
+    },
+    {
+        "id":9,
+        "nombre": "Cirugía De Cabeza, Cuello y Maxilofacial",
+    },
+    {
+        "id":10,
+        "nombre": "Cirugía de Mamas y Tumores",
+    },
+    {
+        "id":11,
+        "nombre": "Cirugía de Mano",
+    },
+
+];
---
+++ utils/sedes.js
@@ -0,0 +1,22 @@
+export const sedes = [
+    {
+        "id":1,
+        "nombre": "Lima",
+    },
+    {
+        "id":2,
+        "nombre": "Mediocentro San Isidro",
+    },
+    {
+        "id":3,
+        "nombre": "San borja",
+    },
+    {
+        "id":4,
+        "nombre": "Surco",
+    },
+    {
+        "id":5,
+        "nombre": "Toda la red de Clínica",
+    },
+]
